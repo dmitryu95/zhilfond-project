4bd92bc92dd01f6d115dfffc77fe99fb98d5a4b1
dmitryu95 <dmitriyu1428@gmail.com>
2024-03-29 14:32:33 +0700

    fix api requests

--- src/api/users.js
+++ src/api/users.js
@@ -1,35 +1,30 @@
 import axios from 'axios'
-import index from "vuex";
 
 const baseURL = process.env.VUE_APP_API_URL
-
 const axiosInstance = axios.create({
     baseURL
 })
 export const getUsersById = (ids) => {
     let idsString = String(ids)
-    let queryParams = idsString.split(', ')
-        .map( (id, index) => ( index === 0) ? 'id=' + id : '&id=' + id ).join('')
+    let idsStr = idsString.split(', ')
 
-    return axiosInstance.get(`/users?${queryParams}`);
+    let queryParams = {
+        params: {
+            id: idsStr
+        }
+    }
+
+    return axiosInstance.get(`/users`,queryParams);
 }
 export const getUserByName = (userNames) => {
     let userNamesString = String(userNames)
-
     const userArr = userNamesString.split(', ')
-        .map((user, index) => {
-            const username = user.trim();
 
-            if (username) {
-                return index === 0 ?
-                    'username=' + username[0].toUpperCase() + username.substring(1) :
-                    '&username=' + username[0].toUpperCase() + username.substring(1);
-            } else {
-                return '';
-            }
-        });
+    let queryParams = {
+        params: {
+            username: userArr.map( userName => userName[0].toUpperCase() + userName.substring(1))
+        }
+    }
 
-    let queryParams = userArr.join('')
-
-    return axiosInstance.get(`/users?${queryParams}`);
+    return axiosInstance.get(`/users`, queryParams);
 }
